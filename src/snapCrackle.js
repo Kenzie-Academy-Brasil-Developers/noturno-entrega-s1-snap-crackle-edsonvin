214a0b2d4c1bfaac444ed86fef085ef0b190caf7
edson vinicius <edsonvin1801@gmail.com>
2021-10-19 20:57:52 -0300

    initial commit

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,25 @@
-// Seu código vai aqui
+let mensagem = ""
+function snapCrackle(maxValue){
+    for(let contador = 1; contador <= maxValue; contador++){
+    if(contador === 1){
+        mensagem = mensagem + "Snap"
+
+    }
+    else if(contador % 2 != 0 & contador % 5 === 0) {
+        mensagem = mensagem + ", SnapCrackle"
+
+    }
+    else if (contador % 5 === 0) {
+        mensagem = mensagem + ", Crackle"
+    }   
+    else if (contador % 2 != 0) {
+        mensagem = mensagem + ", Snap"
+    } else {
+        mensagem = mensagem + ", " + contador
+    }
+
+}
+console.log(mensagem)
+}
+
+snapCrackle(12)
---
+++ src/teste.js
@@ -0,0 +1,8 @@
+let mensagem = ""
+function aFuncao(valor){
+    if(valor % 2 != 0){
+        console.log(mensagem + "snap")
+    }
+}
+
+return aFunção(3)
